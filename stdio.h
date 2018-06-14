43732f30c541424927b545bcd7d54e13d8ea2f0b
Benji Dial <benji3.141@gmail.com>
2018-06-14 10:46:30 -0400

    getchar and gets

--- stdio.h
+++ stdio.h
@@ -4,10 +4,38 @@
   PortlandCLib stdio.h*/
 
 inline void putchar(uint8_t ch) {
-  asm volatile ("pushb %ch\nint 0x8d\naddw $1, %sp");
+  asm volatile (
+    "pushb %ch\n"
+    "int $0x8d\n"
+    "addw $1, %sp"
+  );
 }
 
 inline void puts(uint8_t *str) {
-  asm volatile ("pushw %str\nint 0x8e\naddw $2, %sp");
+  asm volatile (
+    "pushw %str\n"
+    "int $0x8e\n"
+    "addw $2, %sp"
+  );
   putchar('\n');
+}
+
+inline uint8_t getchar(void) {
+  uint8_t r;
+  asm volatile (
+    "pushb $0x01\n"
+    "int $0x99\n"
+    "addw $1, %sp"
+  : "=a" (r));
+  return r;
+}
+
+inline uint8_t *gets(uint8_t *str) {
+  asm volatile (
+    "pushb $0x01"
+    "pushw %str\n"
+    "int $0x9a\n"
+    "addw $3, %sp"
+  );
+  return str;
 }
