1b0ce6ad647904b2f972d26591c8ff9675c11f34
Benji Dial <benji3.141@gmail.com>
2018-07-29 13:25:22 -0400

    Update for portland rewrite

--- stdlib.h
+++ stdlib.h
@@ -4,35 +4,50 @@
   PortlandCLib stdlib.h*/
 
 inline void *calloc(uint16_t size) {
-  void *r = malloc(size);
-  uint8_t *i, *e = (i = r - 1) + size;
-  while ((++i) < e)
-    *i = 0x00;
+  void *r;
+  asm (
+    "int $0x58\n"
+    "mov %ebx, %edx\n"
+    ".loop:\n"
+    "cmpw $4, %ax\n"
+    "jl .done\n"
+    "xord (%ebx), (%ebx)\n"
+    "addd $4, %ebx\n"
+    "subw $4, %ax\n"
+    "jmp .loop\n"
+    ".done:"
+    "cmpw $0, %ax\n"
+    "je .ret\n"
+    "xorb (%ebx), (%ebx)\n"
+    "cmpw $1, %ax\n"
+    "je .ret\n"
+    "xorb (%ebx)1, (%ebx)1\n"
+    "compw $2, %ax\n"
+    "je .ret\n"
+    "xorb (%ebx)2, (%ebx)2\n"
+    ".ret:"
+  : "=edx" (r) : "ax" (size) : "ax", "ebx");
   return r;
 }
 
 inline void *malloc(uint16_t size) {
   void *r;
-  asm volatile (
-    "pushw %size\n"
-    "int $0x80\n"
-    "addw $2, %sp"
-  : "=a" (r));
+  asm (
+    "int $0x58"
+  : "=ebx" (r) : "ax" (size));
   return r;
 }
 
-inline void free(void *block) {
-  asm volatile (
-    "pushw %block\n"
-    "int $0x81\n"
-    "addw $2, %sp"
-  );
+inline void free(void *start) {
+  asm (
+    "int $0x59"
+  :: "ebx" (start) : "al");
 }
 
-inline void system(uint8_t *path) {
-  asm volatile (
-    "pushw %path\n"
-    "int $0x95\n"
-    "addw $2, %sp"
-  );
+inline uint32_t system(uint8_t *filename) {
+  uint32_t r;
+  asm (
+    "int $0x46"
+  : "=edx" (r) : "ebx" (filename) : "al");
+  return r;
 }
